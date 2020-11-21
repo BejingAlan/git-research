d4f563e994dd99deb9edfa1be95dc9a421afddbd
2465861658@qq.com <Wang1991#>
2020-11-21 18:49:10 +0800

    添加个人分支

--- src/main/java/cn/fanzhi/App.java
+++ src/main/java/cn/fanzhi/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("添加开发分支dev222");
         System.out.println("修改代码1");
         System.out.println( "Hello World!" );
     }
