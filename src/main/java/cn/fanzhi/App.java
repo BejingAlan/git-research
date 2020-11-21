663a5b6a030fa167f1a2eb26323e2e6f29f0e57d
2465861658@qq.com <Wang1991#>
2020-11-21 22:31:32 +0800

    提交内容

--- src/main/java/cn/fanzhi/App.java
+++ src/main/java/cn/fanzhi/App.java
@@ -5,9 +5,7 @@ package cn.fanzhi;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("This is dev branch");
-        System.out.println("测试忽略文件");
-        System.out.println("修改代码1111");
+        System.out.println("This is feature_20201121 branch");
         System.out.println("Hello World!");
     }
 }
