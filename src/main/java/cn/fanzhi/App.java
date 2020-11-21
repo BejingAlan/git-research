4c4486a31f8ade98e737faa26a08e3ff9030874f
2465861658@qq.com <Wang1991#>
2020-11-21 20:05:21 +0800

    yyy

--- src/main/java/cn/fanzhi/App.java
+++ src/main/java/cn/fanzhi/App.java
@@ -5,6 +5,7 @@ package cn.fanzhi;
  */
 public class App {
     public static void main(String[] args) {
+        System.out.println("测试忽略文件");
         System.out.println("修改代码1111");
         System.out.println("Hello World!");
     }
