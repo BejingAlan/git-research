da50a76a19e727bc57c068de1eea6d6b2c52f5a1
2465861658@qq.com <Wang1991#>
2020-11-21 17:17:55 +0800

    初始化项目

--- src/main/java/cn/fanzhi/App.java
+++ src/main/java/cn/fanzhi/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("修改");
+        System.out.println("修改代码1");
         System.out.println( "Hello World!" );
     }
 }
